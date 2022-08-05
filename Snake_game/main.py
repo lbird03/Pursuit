821e18785f676fe16a99acb53caaf62b83d5ec36
bird <landon.bird80@gmail.com>
2022-08-04 21:15:48 -0500

    Added Interactable Actor for player
    - Movable player character
    - time based score system
    - proper response for hitting wall

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -3,14 +3,14 @@ from pygame.locals import *
 import time
 import random
 
-#Top Possible Score = 320
 # size of snake unit/grid unit
 SIZE = 50
 BACKGRND_COL = (16,115,235)
 
+# standard food activity
 class Food:
     def __init__(self, parent_screen):
-        self.image = pygame.image.load('food.png')
+        self.image = pygame.image.load('Snake_game/food.png')
         self.parent_screen = parent_screen
         self.x = 150
         self.y = 150
@@ -25,11 +25,62 @@ class Food:
         self.x = random.randint(0,19)*SIZE
         self.y = random.randint(0,15)*SIZE
 
+# Player Activity
+class Player:
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load('Snake_game/food.png')
+        self.parent_screen = parent_screen
+        self.direction = "right"
+        self.x = 400
+        self.y = 400
+    
+    # draw player
+    def draw(self):
+        self.parent_screen.fill(BACKGRND_COL)
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move_left(self):
+        self.direction = 'left'
+
+    def move_right(self):
+        self.direction = 'right'
+
+    def move_up(self):
+        self.direction = 'up'
+    
+    def move_down(self):
+        self.direction = 'down'
+
+    # teleport player to opposite side depending on which wall is hit
+    def hit_limit(self):
+        if self.player.x >= 1000:
+            self.player.x = 0
+        if self.player.x < 0:
+            self.player.x = 1000
+        if self.player.y >= 800:
+            self.player.y = 0
+        if self.player.y < 0:
+            self.player.y = 800
+
+    # keep player moving in current direction
+    def walk(self):
+        if self.direction == 'left':
+            self.x -= 50
+        if self.direction == 'right':
+            self.x += 50
+        if self.direction == 'up':
+            self.y -= 50
+        if self.direction == 'down':
+            self.y += 50
+        self.draw()
+
+
 class Snake:
     def __init__(self, surface, length):
         self.length = length
         self.parent_screen = surface
-        self.block = pygame.image.load("block.png")
+        self.block = pygame.image.load("Snake_game/block.png")
         self.y = [SIZE]*length
         self.x = [SIZE]*length
         self.direction = "right"
@@ -80,10 +131,13 @@ class Game:
         pygame.init()
         self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill(BACKGRND_COL)
-        self.snake = Snake(self.surface, 1)
-        self.snake.draw()
+        # self.snake = Snake(self.surface, 1)
+        # self.snake.draw()
         self.food = Food(self.surface)
         self.food.draw()
+        self.player = Player(self.surface)
+        self.player.draw()
+        self.score = 0
 
     # takes location of food and snake head and returns true upon collision
     def is_collision(self, x1, y1, x2, y2):
@@ -94,75 +148,87 @@ class Game:
 
     # updates screen (food location, snake location, score)
     def play(self):
-        self.snake.walk()
+        # self.snake.walk()
+        self.player.walk()
         self.food.draw()
         self.display_score()
         pygame.display.flip()
 
         # snake collision with food
-        if self.is_collision(self.snake.x[0], self.snake.y[0], self.food.x, self.food.y):
-            self.snake.increase_length()
-            self.food.move()
+        # if self.is_collision(self.snake.x[0], self.snake.y[0], self.food.x, self.food.y):
+        #     self.snake.increase_length()
+        #     self.food.move()
 
         #snake collision with self
-        for i in range(2,self.snake.length):
-            if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
-                raise "Collision Occured"
-            else: pass
+        # for i in range(2,self.snake.length):
+        #     if self.is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+        #         raise "Collision Occured"
+        #     else: pass
 
-    #
+    # displays current score in top right corner
     def display_score(self):
         font = pygame.font.SysFont('arial',30)
-        score = font.render(f"Score: {self.snake.length}", True, (255, 255, 255))
+        score = font.render(f"Score: {self.score}", True, (255, 255, 255))
         self.surface.blit(score, (800,10))
 
+    # display game over and final score
     def show_game_over(self):
         self.surface.fill(BACKGRND_COL)
         font = pygame.font.SysFont('arial',30)
-        msg = font.render(f"Game over! Your score is {self.snake.length}", True, (255, 255, 255))
+        msg = font.render(f"Game over! Your score is {self.score}", True, (255, 255, 255))
         self.surface.blit(msg, (200,300))
-        msg2 = font.render("To play again press enter. To exit, press Esc", True, (255, 255, 255))
+        msg2 = font.render("To play again press Enter/Return. To exit, press Esc", True, (255, 255, 255))
         self.surface.blit(msg2, (200, 350))
 
         pygame.display.flip()
 
     def reset(self):
-        self.snake = Snake(self.surface, 1)
+        # self.snake = Snake(self.surface, 1)
         self.food = Food(self.surface)
-    # defines movement, button actions, and snake speed
+        self.player = Food(self.surface)
+        self.score = 0
+
+    # defines movement, button actions, and movement speed
     # constantly happening loop
     def run(self):
         running = True
         pause = False
+        i = 0
         while running:
+            i += 1
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
+                    # Escape Key
                     if event.key == K_ESCAPE:
                         running = False
                     
+                    # Enter Key
                     if event.key == K_RETURN:
                         pause = False
-
+                    
+                    # Movement keys
                     if not pause:
                         if event.key == K_UP:
-                            self.snake.move_up()
+                            self.player.move_up()
 
                         if event.key == K_DOWN:
-                            self.snake.move_down()
+                            self.player.move_down()
 
                         if event.key == K_LEFT:
-                            self.snake.move_left()
+                            self.player.move_left()
 
                         if event.key == K_RIGHT:
-                            self.snake.move_right()
+                            self.player.move_right()
 
+                # if player clicks X in corner
                 elif event.type == QUIT:
                     running = False
                 
-                if self.snake.x[0] >= 1000 or self.snake.x[0] < 0 or self.snake.y[0] >= 800 or self.snake.y[0] < 0:
-                    self.show_game_over()
-                    pause = True
-                    self.reset()
+                #if player runs into wall
+                if self.player.x >= 1000 or self.player.x < 0 or self.player.y >= 800 or self.player.y < 0:
+                    self.player.hit_limit()
+
+            # update actor positions if not game over
             try:
                 if not pause:
                     self.play()
@@ -171,7 +237,10 @@ class Game:
                 pause = True
                 self.reset()
 
-            time.sleep(0.1)
+            # increment score every second
+            if i % 5 == 0:
+                self.score += 1
+            time.sleep(0.2)
 
 
     
